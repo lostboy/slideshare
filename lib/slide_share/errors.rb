581d2230ab28a6ae5fe80a75547768e2fd4ae700
Russell Norris <rsl@sneaky.local>
2009-01-15 10:02:09 -0500

    added inheritance for errors

--- lib/slide_share/errors.rb
+++ lib/slide_share/errors.rb
@@ -1,17 +1,18 @@
 module SlideShare
-  # Raised when API method requires username and password but none
-  # was provided
-  class InsufficientPermission < StandardError; end
-  
-  # Raised when API method was given incorrect username and/or password
-  class FailedUserAuthentication < StandardError; end
-  
-  # Raised when no slideshow matches the supplied slideshow_id
-  class SlideshowNotFound < StandardError; end
-  
-  # Raised when the application has made more than 1000 calls a day
-  class AccountExceededDailyLimit < StandardError; end
-  
+  # Root error
   # Raised when none of the other errors match
   class ServiceError < StandardError; end
+  
+  # Raised when API method requires username and password but none
+  # was provided
+  class InsufficientPermission < ServiceError; end
+  
+  # Raised when API method was given incorrect username and/or password
+  class FailedUserAuthentication < ServiceError; end
+  
+  # Raised when no slideshow matches the supplied slideshow_id
+  class SlideshowNotFound < ServiceError; end
+  
+  # Raised when the application has made more than 1000 calls a day
+  class AccountExceededDailyLimit < ServiceError; end
 end
